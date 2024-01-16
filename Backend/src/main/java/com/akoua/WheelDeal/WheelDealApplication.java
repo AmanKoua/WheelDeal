972a972c1ac7d64d5a92a542653a495f3a0575cf
Aman <akoua@umich.edu>
2024-01-16 17:26:06 -0500

    Backend project setup compelted

---
+++ Backend/src/main/java/com/akoua/WheelDeal/WheelDealApplication.java
@@ -0,0 +1,13 @@
+package com.akoua.WheelDeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelDealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelDealApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/akoua/WheelDeal/WheelDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akoua.WheelDeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelDealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
