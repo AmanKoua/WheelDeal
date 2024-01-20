70c0cfe823d25836739b7c1cf0c4a5d24e4f435b
Aman <akoua@umich.edu>
2024-01-20 16:17:26 -0500

    get all cities endpoint implemented

--- Backend/src/main/java/com/akoua/WheelDeal/City/CityController.java
+++ Backend/src/main/java/com/akoua/WheelDeal/City/CityController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -29,7 +30,12 @@ public class CityController {
         }
 
         return ResponseEntity.ok(city.get());
+    }
 
+    @GetMapping
+    @RequestMapping("/cities")
+    public ResponseEntity<Object> getAllCities(){
+        return ResponseEntity.ok().body(cityRepository.getAllCities());
     }
 
 }
--- Backend/src/main/java/com/akoua/WheelDeal/City/CityRepository.java
+++ Backend/src/main/java/com/akoua/WheelDeal/City/CityRepository.java
@@ -9,4 +9,8 @@ import java.util.List;
 public interface CityRepository extends JpaRepository<City, Long> {
     @Query("SELECT c FROM City c where c.name = :name")
     public List<City> getCityByName(@Param("name") String name);
+
+    @Query("SELECT c FROM City c where c.id > -1")
+    public List<City> getAllCities();
+
 }
