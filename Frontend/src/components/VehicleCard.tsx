affde81cee5653212d3728888437586559d338a6
Aman <akoua@umich.edu>
2024-01-21 18:41:31 -0500

    Vehicle list page completed

--- Frontend/src/components/VehicleCard.tsx
+++ Frontend/src/components/VehicleCard.tsx
@@ -6,8 +6,39 @@ interface Props {
 }
 
 const VehicleCard = ({ vehicle, citiesList }: Props) => {
-  const [ownerDetails, setOwnerDetails] = useState([]);
+  const [ownerDetails, setOwnerDetails] = useState<Object | undefined>(
+    undefined
+  );
   const [cityName, setCityName] = useState("");
+
+  useEffect(() => {
+    if (!vehicle || ownerDetails != undefined) {
+      return;
+    }
+
+    const getOwnerDetails = async () => {
+      const token = localStorage.getItem("token");
+      const response = await fetch(
+        "http://localhost:8080/user?email=" + vehicle.ownerEmail,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        console.error("An error occured when fetching vehicle owner details!");
+        return;
+      }
+
+      const json = await response.json();
+      setOwnerDetails(json);
+    };
+
+    getOwnerDetails();
+  }, [ownerDetails]);
 
   useEffect(() => {
     if (cityName) {
@@ -22,8 +53,75 @@ const VehicleCard = ({ vehicle, citiesList }: Props) => {
     }
   }, []);
 
+  const processPrice = (priceNum: Number): String => {
+    return priceNum.toLocaleString();
+  };
+
+  console.log(vehicle);
+
   return (
-    <div className="bg-red-200 w-3/4 mt-4 ml-auto mr-auto">{cityName}</div>
+    <div className="shadow-md hover:shadow-xl p-1 w-4/6 mt-4 ml-auto mr-auto">
+      <div className="border-b border-gray-200 w-6/6 h-8 ml-auto mr-auto flex flex-row">
+        {ownerDetails && (
+          <div className="w-1/3 h-max mt-auto mb-auto text-center">
+            <strong>Name :</strong> {ownerDetails.firstName}{" "}
+            {ownerDetails.lastName}
+          </div>
+        )}
+        <div className=" w-1/3 h-max mt-auto mb-auto text-center">
+          <strong>City :</strong> {cityName}
+        </div>
+        {ownerDetails && (
+          <div className="w-1/3 h-max mt-auto mb-auto text-center">
+            <strong>Rating :</strong> {ownerDetails.avgRating}
+          </div>
+        )}
+        {ownerDetails && (
+          <div className=" w-1/3 h-max mt-auto mb-auto text-center">
+            <strong>Deal count :</strong> {ownerDetails.dealCount}
+          </div>
+        )}
+      </div>
+      <div className="border-b border-gray-200 w-6/6 h-8 mt-1 ml-auto mr-auto flex flex-row">
+        {vehicle && (
+          <div className=" w-1/3 h-max mt-auto mb-auto text-center">
+            <strong>Make :</strong> {vehicle.make}
+          </div>
+        )}
+        {vehicle && (
+          <div className=" w-1/3 h-max mt-auto mb-auto text-center">
+            <strong>Model :</strong> {vehicle.model}
+          </div>
+        )}
+        {vehicle && (
+          <div className=" w-1/3 h-max mt-auto mb-auto text-center">
+            <strong>Year :</strong> {vehicle.year}
+          </div>
+        )}
+        {vehicle && (
+          <div className=" w-1/3 h-max mt-auto mb-auto text-center">
+            <strong>Color :</strong> {vehicle.color}
+          </div>
+        )}
+      </div>
+      <div className=" w-6/6 h-8 mt-1 ml-auto mr-auto flex flex-row">
+        {vehicle && (
+          <div className=" w-1/2 h-max mt-auto mb-auto text-center">
+            <strong>Category :</strong> {processPrice(vehicle.category)}
+          </div>
+        )}
+        {vehicle && (
+          <div className=" w-1/2 h-max mt-auto mb-auto text-center">
+            <strong>Price :</strong> {processPrice(vehicle.defaultPrice)}
+          </div>
+        )}
+        {vehicle && (
+          <div className=" w-1/2 h-max mt-auto mb-auto text-center">
+            <strong>Condition :</strong> {vehicle.condition}
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
